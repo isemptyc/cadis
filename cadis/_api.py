953ea037f12e1a6364feaa3af3f0b5f4e1046c0c
Isempty <isempty.chiu@gmail.com>
2026-04-03 22:40:24 +0800

    Add Norway support and bump verison to 0.4.3

--- cadis/_api.py
+++ cadis/_api.py
@@ -15,7 +15,7 @@ from .version import __version__
 
 SCHEMA_VERSION = "1"
 VERSION = __version__
-SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE"]
+SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO"]
 
 
 def _infer_resolution_state(
