d5764990776bc634a5c8f0425f896e5107688860
Isempty <isempty.chiu@gmail.com>
2026-04-28 02:03:12 +0800

    release(ca.admin): publish dataset v1.0.0(multilingual_v1)

--- cadis/_api.py
+++ cadis/_api.py
@@ -30,7 +30,7 @@ from .version import __version__
 
 SCHEMA_VERSION = "1"
 VERSION = __version__
-SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "US"]
+SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "US", "CA"]
 DATASET_ISO2_ALIASES = {
     "PR": "US",
     "VI": "US",
