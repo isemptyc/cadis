3b7fa7c84c81ec4ad8434998e2e900b7ec53294b
Isempty <isempty.chiu@gmail.com>
2026-04-29 19:31:24 +0800

    feat(cz): enable Czech Republic dataset support

--- cadis/_api.py
+++ cadis/_api.py
@@ -33,7 +33,7 @@ from .version import __version__
 
 SCHEMA_VERSION = "1"
 VERSION = __version__
-SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "IS", "US", "CA", "AU", "NZ", "CH", "AT", "PL", "LU"]
+SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "IS", "US", "CA", "AU", "NZ", "CH", "AT", "PL", "LU", "CZ"]
 DATASET_ISO2_ALIASES = {
     "PR": "US",
     "VI": "US",
