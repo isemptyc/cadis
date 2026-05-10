3be032e9c7ada5947eab31fab9918b1b6f74ae90
Isempty <isempty.chiu@gmail.com>
2026-05-11 00:00:05 +0800

    feat(in): enable India dataset support

--- cadis/_api.py
+++ cadis/_api.py
@@ -33,7 +33,7 @@ from .version import __version__
 
 SCHEMA_VERSION = "1"
 VERSION = __version__
-SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "IS", "US", "CA", "AU", "NZ", "CH", "AT", "PL", "LU", "CZ", "SG", "MY", "TH", "ID", "GR", "TR", "BR"]
+SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "IS", "US", "CA", "AU", "NZ", "CH", "AT", "PL", "LU", "CZ", "SG", "MY", "TH", "ID", "GR", "TR", "BR", "IN"]
 DATASET_ISO2_ALIASES = {
     "PR": "US",
     "VI": "US",
