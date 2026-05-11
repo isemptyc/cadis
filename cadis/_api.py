745def9118d9f48bc64da0cc6e1a9e7fc65b2b73
Isempty <isempty.chiu@gmail.com>
2026-05-11 12:07:42 +0800

    feat(hr): enable Croatia dataset support

--- cadis/_api.py
+++ cadis/_api.py
@@ -33,7 +33,7 @@ from .version import __version__
 
 SCHEMA_VERSION = "1"
 VERSION = __version__
-SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "IS", "US", "CA", "AU", "NZ", "CH", "AT", "PL", "LU", "CZ", "SG", "MY", "TH", "ID", "GR", "TR", "BR", "IN", "MX", "PH", "VN", "AR", "CL", "CO", "PE", "AL", "BA", "BG"]
+SUPPORTED_ISO2 = ["JP", "TW", "GB", "IT", "KR", "SE", "NO", "DK", "BE", "NL", "FR", "DE", "ES", "PT", "FI", "IS", "US", "CA", "AU", "NZ", "CH", "AT", "PL", "LU", "CZ", "SG", "MY", "TH", "ID", "GR", "TR", "BR", "IN", "MX", "PH", "VN", "AR", "CL", "CO", "PE", "AL", "BA", "BG", "HR"]
 DATASET_ISO2_ALIASES = {
     "PR": "US",
     "VI": "US",
